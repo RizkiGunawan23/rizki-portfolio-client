281bd676009549ed4d7a0a8e2ddcd6ea119391e5
RizkiGunawan23 <rizkigunawan3025@gmail.com>
2025-10-16 16:26:49 +0700

    feat: improve footer

--- src/features/public/components/layout/Footer.tsx
+++ src/features/public/components/layout/Footer.tsx
@@ -1,5 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="min-h-52 bg-red-50 text-white">This is footer</footer>
+    <footer className="flex min-h-12 items-center justify-center bg-[#2d2f33] text-white">
+      <span>© 2025 | Rizki Portfolio</span>
+    </footer>
   );
 }
