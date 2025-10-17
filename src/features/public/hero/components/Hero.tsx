27c8bc5662395b1b0f8bdfdd65c89eafee871673
RizkiGunawan23 <rizkigunawan3025@gmail.com>
2025-10-17 13:51:05 +0700

    feat: add 5 frameworks in hero section

--- src/features/public/hero/components/Hero.tsx
+++ src/features/public/hero/components/Hero.tsx
@@ -6,19 +6,38 @@ export default function Hero() {
       <div>
         <Image src="/Hero Logo.png" alt="Hero Logo" width={500} height={500} />
       </div>
-      <div className="space-y-2">
+      <div className="space-y-3">
         <h2 className="text-7xl font-bold text-white">
           Hi, I&apos;m Rizki <span className="text-[#71eaca]">.</span>
         </h2>
         <h2 className="bg-gradient-to-r from-[#7f98b6] to-[#535e6c] bg-clip-text text-5xl font-bold text-transparent">
           Backend Developer
         </h2>
-        <div>
+        <div className="flex space-x-3">
           <Image
             alt="Spring Logo"
             src={"/Spring Logo.png"}
-            width={30}
-            height={30}
+            width={50}
+            height={1}
+          />
+          <Image
+            alt="Express Logo"
+            src={"/Express Logo.png"}
+            width={125}
+            height={10}
+          />
+          <Image
+            alt="Django REST Framework Logo"
+            src={"/Django REST Framework Logo.png"}
+            width={100}
+            height={1}
+          />
+          <Image alt="Next Logo" src={"/Next Logo.png"} width={75} height={1} />
+          <Image
+            alt="React Logo"
+            src={"/React Logo.png"}
+            width={55}
+            height={1}
           />
         </div>
       </div>
